b4f06b76777e12b742366b7d16e7aed85e95c0c9
Isabella <142615084+Isacrff@users.noreply.github.com>
2024-10-24 08:34:30 -0300

    d

--- perguntas.js
+++ perguntas.js
@@ -1,120 +1,120 @@
 criaCartao(
-    ‘Literatura Brasileira’,
-    ‘Quem foi Machado de Assis?’,
-    ‘Escritor brasileiro’
+    'Literatura Brasileira',
+    'Quem foi Machado de Assis?'',
+    'Escritor brasileiro'
 )
     
     criaCartao(
-    ‘Filosofia’,
-    ‘O que é o estoicismo?’,
-    ‘Escola filosófica’
+    'Filosofia',
+    'O que é o estoicismo?',
+    'Escola filosófica'
 )
     
     criaCartao(
-    ‘História’,
-    ‘Quando aconteceu a Revolução Francesa?’,
-    ‘Em 1789’
+    'História',
+    'Quando aconteceu a Revolução Francesa?',
+    'Em 1789'
 )
     
     criaCartao(
-    ‘Geografia’,
-    ‘Qual é o maior país do mundo?’,
-    ‘Rússia’
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'Rússia'
 )
     
     criaCartao(
-    ‘Ciência’,
-    ‘O que é a gravidade?’,
-    ‘Força de atração’
+    'Ciência',
+    'O que é a gravidade?',
+    'Força de atração'
 )
     
     criaCartao(
-    ‘Matemática’,
-    ‘Qual é o valor de π?’,
-    ‘Aproximadamente 3,14’
+    'Matemática',
+    'Qual é o valor de π?',
+    'Aproximadamente 3,14'
 )
     
     criaCartao(
-    ‘Biologia’,
-    ‘O que é DNA?’,
-    ‘Material genético’
+    'Biologia',
+    'O que é DNA?',
+    'Material genético'
 )
     
     criaCartao(
-    ‘Química’,
-    ‘O que é H₂O?’,
-    ‘Água’
+    'Química',
+    'O que é H₂O?',
+    'Água'
 )
     
     criaCartao(
-    ‘Física’,
-    ‘O que é velocidade?’,
-    ‘Distância/tempo’
+    'Física',
+    'O que é velocidade?',
+    'Distância/tempo'
 )
 
     
     criaCartao(
-    ‘Astronomia’,
-    ‘Qual é o maior planeta do Sistema Solar?’,
-    ‘Júpiter’
+    'Astronomia',
+    'Qual é o maior planeta do Sistema Solar?',
+    'Júpiter'
 )
     
     criaCartao(
-    ‘Tecnologia’,
-    ‘O que é IA?’,
-    ‘Inteligência Artificial’
+    'Tecnologia',
+    'O que é IA?',
+    'Inteligência Artificial'
 )
     
     criaCartao(
-    ‘Música’,
-    ‘Quem é o autor de "Para Elise"?’,
-    ‘Beethoven’
+    'Música',
+    'Quem é o autor de "Para Elise"?',
+    'Beethoven'
 )
     
     criaCartao(
-    ‘Arte’,
-    ‘Quem pintou a Mona Lisa?’,
-    ‘Da Vinci’
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    'Da Vinci'
 )
     
     criaCartao(
-    ‘Política’,
-    ‘O que é democracia?’,
-    ‘Governo do povo’
+    'Política',
+    'O que é democracia?',
+    'Governo do povo'
 )
     
     criaCartao(
-    ‘Economia’,
-    ‘O que é inflação?’,
-    ‘Aumento de preços’
+    'Economia',
+    'O que é inflação?',
+    'Aumento de preços'
 )
     
     criaCartao(
-    ‘Sociologia’,
-    ‘O que é sociedade?’,
-    ‘Grupo de pessoas’
+    'Sociologia',
+    'O que é sociedade?',
+    'Grupo de pessoas'
 )
     
     criaCartao(
-    ‘Psicologia’,
-    ‘O que é inconsciente?’,
-    ‘Parte oculta da mente’
+    'Psicologia',
+    'O que é inconsciente?',
+    'Parte oculta da mente'
 )
     
-    criaCaraão(
-    ‘Linguística’,
-    ‘O que é morfologia?’,
-    ‘Estudo das palavras’
-)
-    
-    criaCartao(
-    ‘Religião’,
-    ‘Qual é o livro sagrado dos cristãos?’,
-    ‘A Bíblia’
+    criaCartão(
+    'Linguística',
+    'O que é morfologia?',
+    'Estudo das palavras'
 )
     
     criaCartao(
-    ‘Mitologia’,
-    ‘Quem é o deus do trovão na mitologia nórdica?’,
-    ‘Thor’
+    'Religião',
+    'Qual é o livro sagrado dos cristãos?',
+    'A Bíblia'
+)
+    
+    criaCartao(
+    'Mitologia',
+    'Quem é o deus do trovão na mitologia nórdica?',
+    'Thor'
 )
